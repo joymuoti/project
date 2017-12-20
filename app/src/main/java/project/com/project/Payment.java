9fa1609293e35bf53be10a0e9866dd5721995cea
Eokinyi35 <Emmanuel.okinyi@strathmore.edu>
2017-12-20 08:57:53 +0300

    Logout

--- app/src/main/java/project/com/project/Payment.java
+++ app/src/main/java/project/com/project/Payment.java
@@ -1,5 +1,6 @@
 package project.com.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +10,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Payment extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth firebaseAuth;
-        Button btnLogout;
+    protected  Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-
+        firebaseAuth=FirebaseAuth.getInstance();
         btnLogout = (Button) findViewById(R.id.btnLogout);
 
         //adding listener to button
@@ -26,12 +27,14 @@ public class Payment extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         //if logout is pressed
-        if(view == btnLogout){
+       // if(view == btnLogout){
             //logging out the user
+            finish();
             firebaseAuth.signOut();
             //closing activity
-            finish();
+        Intent Payment = new Intent(Payment.this, Login.class);
+        startActivity(Payment);
         }
-    }
+    //}
     }
 
